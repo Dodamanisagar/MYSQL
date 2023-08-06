190e7143b0e73ab28ce38558c79c32841cb652a2
Sagar Dodamani <sagardodamani235@gmail.com>
2023-08-06 16:18:32 +0530

    Update 4.TRUNCATE.sql

--- 1-Data Defination Language(DDL)/4.TRUNCATE.sql
+++ 1-Data Defination Language(DDL)/4.TRUNCATE.sql
@@ -2,4 +2,4 @@
 -- It is used to remove all the records from the table.
 truncate TABLE stack2;
 
-truncate TABLE SAGAR.PERSON_3; -- Removing all the recods fromperson_3 table which is present in sagar DB.
+truncate TABLE SAGAR.PERSON_3; -- Removing all the recods from person_3 table which is present in sagar DB.
