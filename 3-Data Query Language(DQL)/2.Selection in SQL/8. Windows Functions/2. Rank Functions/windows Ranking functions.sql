667acfabfcaac455d712d6068a47c754b4ce4fff
Sagar Dodamani <sagardodamani235@gmail.com>
2023-10-15 17:22:02 +0530

    Update windows Ranking functions.sql

--- 3-Data Query Language(DQL)/2.Selection in SQL/8. Windows Functions/2. Rank Functions/windows Ranking functions.sql
+++ 3-Data Query Language(DQL)/2.Selection in SQL/8. Windows Functions/2. Rank Functions/windows Ranking functions.sql
@@ -1,4 +1,4 @@
--- ranking functions winthout partition by and order by
+-- ranking functions without partition by and order by
 select *,
 	rank() over() as rankNo,
 	dense_rank() over() as dense_rankNo,
@@ -6,7 +6,7 @@ select *,
     percent_rank() over() as pRank
 FROM EMP;
 
--- ranking functions winth partition by
+-- ranking functions with partition by
 select *,
 	rank() over(partition by deptno ) as rankNo,
 	dense_rank() over(partition by deptno ) as dense_rankNo,
@@ -14,7 +14,7 @@ select *,
     percent_rank() over(partition by deptno ) as pRank
 FROM EMP;
 
--- ranking functions wint partition by and order by
+-- ranking functions with partition by and order by
 select *,
 	rank() over(partition by deptno order by ename) as rankNo,
 	dense_rank() over(partition by deptno order by ename) as dense_rankNo,
